dadb839c952007f73b307dfb233eceb06c7f4220
UnluckyNinja <unluckyninja1994@gmail.com>
2015-01-11 01:33:49 +0800

    delete old files

--- src/asciiPanel/AsciiCharacterData.java
+++
@@ -1,9 +0,0 @@
-package asciiPanel;
-
-import com.badlogic.gdx.graphics.Color;
-
-public class AsciiCharacterData {
-	public char character;
-	public Color foregroundColor;
-	public Color backgroundColor;
-}

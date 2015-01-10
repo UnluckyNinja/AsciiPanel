8230d6e2e04956acf4cc2f6f246d64be29656cdd
UnluckyNinja <unluckyninja1994@gmail.com>
2015-01-11 01:31:45 +0800

    port to libgdx done.

---
+++ src/asciiScreen/AsciiCharacterData.java
@@ -0,0 +1,9 @@
+package asciiscreen;
+
+import com.badlogic.gdx.graphics.Color;
+
+public class AsciiCharacterData {
+	public char character;
+	public Color foregroundColor;
+	public Color backgroundColor;
+}
---
+++ src/asciiScreen/TileTransformer.java
@@ -0,0 +1,5 @@
+package asciiscreen;
+
+public interface TileTransformer {
+	public void transformTile(int x, int y, AsciiCharacterData data);
+}
